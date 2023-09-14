86309c3b2bd5c6493a089482b55bcde87b57b6d3
Matvey-Makaro <matvey.makaro@gmail.com>
2023-09-14 15:01:15 +0300

    Task 1 done.

--- z1/main.cpp
+++ z1/main.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <sstream>
+#include <algorithm>
+#include "test_runner.h"
 
-int main()
+void print_hello_world(std::ostream& out)
 {
     srand(time(nullptr));
 
-    std::cout << "Hello, world!\n";
-    std::cout << "Andhiagain!\n";
+    out << "Hello, world!\n";
+    out << "Andhiagain!\n";
 
     int marks = rand() % 46 + 5;
     for (int i = 0; i < marks; i++)
     {
-        std::cout << "!";
+        out << "!";
     }
-    std::cout << "\n";
+    out << "\n";
+}
+
+void test_print_hello_world()
+{
+    std::stringstream ss;
+    print_hello_world(ss);
+
+    std::string first_line, second_line, third_line;
+    getline(ss, first_line);
+    getline(ss, second_line);
+    getline(ss, third_line);
+    int num_of_exclamation_mark = std::count(third_line.cbegin(), third_line.cend(), '!');
+    ss.get();
+
+    ASSERT(ss.eof())
+    ASSERT(first_line == "Hello, world!");
+    ASSERT(second_line == "Andhiagain!");
+    ASSERT(num_of_exclamation_mark == third_line.size())
+    ASSERT(5 <= num_of_exclamation_mark);
+    ASSERT(num_of_exclamation_mark <= 50);
+}
+
+int main()
+{
+    TestRunner tr;
+    RUN_TEST(tr, test_print_hello_world);
+
+    print_hello_world(std::cout);
 
     return 0;
 }
---
+++ z1/test_runner.h
@@ -0,0 +1,110 @@
+#pragma once
+#include <sstream>
+#include <exception>
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    os << "(";
+    bool first = true;
+    for (const auto& x : vec)
+    {
+        if (!first)
+            os << ",";
+        first = false;
+        os << x;
+    }
+    return os << ")";
+}
+
+template <class T>
+std::ostream& operator << (std::ostream& os, const std::set<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& x : s) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << x;
+    }
+    return os << "}";
+}
+
+template <class K, class V>
+std::ostream& operator << (std::ostream& os, const std::map<K, V>& m) {
+    os << "{";
+    bool first = true;
+    for (const auto& kv : m) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << kv.first << ": " << kv.second;
+    }
+    return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u,
+    const std::string& hint)
+{
+    if (t != u) {
+        std::ostringstream os;
+        os << "Assertion failed: " << t << " != " << u
+            << " hint: " << hint;
+        throw std::runtime_error(os.str());
+    }
+}
+
+inline void Assert(bool b, const std::string& hint) {
+    AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+    template <class TestFunc>
+    void RunTest(TestFunc func, const std::string& test_name) const {
+        try {
+            func();
+            std::cerr << test_name << " OK" << std::endl;
+        }
+        catch (std::runtime_error& e) {
+            ++fail_count;
+            std::cerr << test_name << " fail: " << e.what() << std::endl;
+        }
+    }
+
+    ~TestRunner() {
+        if (fail_count > 0) {
+            std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
+            exit(1);
+        }
+    }
+
+private:
+    mutable int fail_count = 0;
+};
+
+#define ASSERT_EQUAL(x, y) {                       \
+  std::ostringstream assertEqualOS;                \
+  assertEqualOS << #x << " != " << #y << ", "      \
+    << __FILE__ << ":" << __LINE__;                \
+  AssertEqual(x, y, assertEqualOS.str());          \
+}
+
+#define ASSERT(x){                                 \
+    std::ostringstream assertEqualOS;              \
+    assertEqualOS << #x << "is false, "            \
+    << __FILE__ << ":" << __LINE__;                \
+    Assert(x, assertEqualOS.str());                \
+} 
+
+#define RUN_TEST(tr, func) \
+    tr.RunTest(func, #func)
