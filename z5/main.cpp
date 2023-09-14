a63492eaf93ff06abbeeaa92974c58e850f372e9
aneroid11 <a-e-v2003@yandex.by>
2023-09-14 14:42:23 +0300

    loop over files in directory

--- z5/main.cpp
+++ z5/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
+
+using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (argc != 3)
+    {
+        std::cout << "please specify the directory AND the extension!\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::string path_to_dir = argv[1];
+    std::string ext = argv[2];
+
+    for (const auto& dir_entry : recursive_directory_iterator(path_to_dir))
+    {
+        std::cout << dir_entry << std::endl;
+    }
+
+    std::cout << "find files with extension " << argv[2] << " in " << argv[1] << "\n";
     return 0;
 }
