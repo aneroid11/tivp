7d65533fb3d802b12a0481753a6a6df2e95d7bb7
aneroid11 <a-e-v2003@yandex.by>
2023-09-15 14:10:43 +0300

    curl

--- z6/main.cpp
+++ z6/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <string>
+#include <cstdio>
+#include <curl/curl.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (argc != 3)
+    {
+        std::cout << "you must specify exactly two arguments: url and path!\n";
+        exit(EXIT_FAILURE);
+    }
+
+    const std::string url = argv[1];
+    const std::string path = argv[2];
+
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    curl = curl_easy_init();
+
+    if (curl)
+    {
+        fp = fopen(path.c_str(),"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+        res = curl_easy_perform(curl);
+        if (res)
+        {
+            std::cerr << "cannot download file from url!\n";
+            exit(EXIT_FAILURE);
+        }
+
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
+    else
+    {
+        std::cerr << "failed to initialize CURL!\n";
+    }
+
     return 0;
 }
